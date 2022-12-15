7fb484c6d3643b073fa52f234ecc3deb15e671ad
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-15 15:29:25 +0000

    Adding tarfile member sanitization to extractall()

--- maxmind.py
+++ maxmind.py
@@ -95,7 +95,26 @@ def main():
         logger.info("extracting files to: '%s/'", options.path)
         if tarfile.is_tarfile(filepath):
             with tarfile.open(filepath) as tar:
-                tar.extractall(members=binary_files(tar), path=options.path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, members=binary_files(tar), path=options.path)
         elif zipfile.is_zipfile(filepath):
             with zipfile.ZipFile(filepath) as zip:
                 zip.extractall(members=csv_files(zip.infolist()),
